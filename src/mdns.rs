f8563fe14ff760cdfd9f807e016964dde10d9ee2
Jeremy Jack <jeremy@rocketscientry.com>
2026-07-07 20:49:49 -0500

    fix: only use ipv4 for communications

--- src/mdns.rs
+++ src/mdns.rs
@@ -1,11 +1,13 @@
 use anyhow::Result;
-use mdns_sd::{ServiceDaemon, ServiceInfo};
+use mdns_sd::{IfKind, ServiceDaemon, ServiceInfo};
 use std::collections::HashMap;
 
 pub fn start_advertisement(config: &crate::config::Config, mac: &str, address: &str) -> Result<(ServiceDaemon, ServiceInfo)> {
     let port = config.daemon.as_ref().map(|d| d.port).unwrap_or(crate::config::DEFAULT_DAEMON_PORT);
 
     let mdns = ServiceDaemon::new()?;
+    mdns.disable_interface(IfKind::IPv6)?;
+
     let service_type = "_grubstation._tcp.local.";
     let instance_name = address.to_string();
     let system_hostname = hostname::get()?.to_string_lossy().into_owned();
@@ -19,7 +21,7 @@ pub fn start_advertisement(config: &crate::config::Config, mac: &str, address: &
         service_type,
         &instance_name,
         &host_name,
-        "", // Use automatic address detection
+        address,
         port,
         Some(properties),
     )?.enable_addr_auto();
